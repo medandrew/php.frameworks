929979de5209ab4a660db686f92409b6a72ab251
Andrew <359747@mail.ru>
2017-08-15 23:40:44 +0700

    Домашнее задание 1

---
+++ laravel5/resources/views/index.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Добро пожаловать</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <!-- Styles -->
+        <link href="/css/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <div class="content">
+                <div class="welcome">Добро пожаловать на новый курс "Современные PHP-фреймворки"</div>
+                <div class="title m-b-md">
+                    Laravel 5.4
+                </div>
+
+                <div class="links">
+                    <a href="http://profit.frameworks.laravel5/">Проект на Laravel5</a>
+                    <a href="http://profit.frameworks.symfony3/">Проект на Symfony3</a>
+                    <a href="http://profit.frameworks.yii2/">Проект на Yii2</a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
